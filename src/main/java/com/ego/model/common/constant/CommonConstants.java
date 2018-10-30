0534a385f1f423d5053b0405c25a4fc3cb27dae3
ego <157026488@qq.com>
2018-10-30 13:55:24 +0800

    添加分布插件

---
+++ src/main/java/com/ego/model/common/constant/CommonConstants.java
@@ -0,0 +1,19 @@
+package com.ego.model.common.constant;
+
+public interface CommonConstants {
+
+    /** scope: Session,Cookie */
+    String USER_LANGUAGE_NAME = "language";
+
+    // 返回码
+    int RESPONSE_CODE_SUCCESS = 0;
+    int RESPONSE_CODE_FAILURE = 1;
+    int RESPONSE_CODE_RESOURCE_NOT_EXIST = 2;
+    int RESPONSE_CODE_ANONYMOUS = 10;
+    int RESPONSE_CODE_ACCESS_DENIED = 20;
+    int RESPONSE_CODE_LOGIN_OFF = 100;
+
+    // 分页参数
+    int DEFAULT_PAGE_NUMBER = 0; // 默认页数，0代表第一页
+    int DEFAULT_PAGE_SIZE = 15; // 默认分页大小，15
+}
