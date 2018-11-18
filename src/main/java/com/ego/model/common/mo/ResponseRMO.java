7ec457a3663b032da2941bc850cb181f3f10d029
ego <157026488@qq.com>
2018-11-18 15:35:38 +0800

    多个环境配置文件

---
+++ src/main/java/com/ego/model/common/mo/ResponseRMO.java
@@ -0,0 +1,61 @@
+package com.ego.model.common.mo;
+
+
+import com.ego.model.common.constant.CommonConstants;
+
+import java.io.Serializable;
+
+public class ResponseRMO implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    private int code;
+
+    private Object data;
+
+    private String msg="";
+
+    public ResponseRMO(){
+
+    }
+
+    public boolean success() {
+        return CommonConstants.RESPONSE_CODE_SUCCESS == code;
+    }
+
+    public ResponseRMO(int code, String msg){
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public ResponseRMO(int code, String msg, Object data){
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
